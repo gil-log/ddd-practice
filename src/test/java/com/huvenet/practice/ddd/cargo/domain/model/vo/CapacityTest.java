e7a5ca787e35a8156360e58613ddf4ed6e43d671
Gillog <swgil007@naver.com>
2022-12-06 12:12:56 +0900

    [#15] VO Code 비교 작성

---
+++ src/test/java/com/huvenet/practice/ddd/cargo/domain/model/vo/CapacityTest.java
@@ -0,0 +1,10 @@
+package com.huvenet.practice.ddd.cargo.domain.model.vo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacityTest {
+
+}
